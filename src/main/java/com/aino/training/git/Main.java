2fc9a604cf2710d22715c9718411be776ea04cf7
iternia <iternia@PC-3>
2017-01-19 10:23:56 +0530

    function added
    second display function added...

--- src/main/java/com/aino/training/git/Main.java
+++ src/main/java/com/aino/training/git/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("I am in Main");
-
+		
+		new Main().display();
+	}
+	void display()
+	{
+		System.out.println("hello i am in second function");
 	}
 
 }
